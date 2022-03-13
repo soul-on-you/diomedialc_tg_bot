cf0ee712ec031b15ce1c55e76cf1e3b222442a3f
brendan <superden081@gmail.com>
2022-03-13 23:39:00 +0300

    added admin options to create edit and delete rows in currencies db

--- js/bot.js
+++ js/bot.js
@@ -7,13 +7,13 @@ import sequelize from "./db_connection";
 import {
   Users as UsersModel,
   UserСurrencies as UserСurrenciesModel,
-  Currency as CurrencyModel,
+  Сurrencies as CurrencyModel,
 } from "./db_models";
 import BotOptions from "./bot_options";
 
 const bot = new TelegramBot(token, { polling: true });
 
-const states={};
+const states = {};
 
 const start = async () => {
   try {
@@ -84,26 +84,98 @@ const start = async () => {
       }
     }
 
-    console.log(msg);
-    const ans = bot.sendMessage(chatID, "Я вас не понимаю");
+    await console.log("STATES:\n", states);
+    await console.log("MESSAGE", msg);
 
-    return console.log((await ans).message_id);
+    if (states[chatID] && states[chatID].runtime == "message") {
+      if (states[chatID].command == "addCurrency") {
+        states[chatID].command = await "";
+        states[chatID].runtime = await "";
+        const [currencyName, currencyAPI] = await msg.text.split(":");
+        // await console.log(currencyName, currencyAPI);
+        try {
+          await CurrencyModel.create({
+            currencyName: currencyName,
+            currencyAPI: currencyAPI,
+          });
+        } catch (e) {
+          if (e.name == "SequelizeUniqueConstraintError") {
+            console.error(e);
+          } else {
+            const eMsg =
+              await "Неудалось создать запись нового пользователя в БД!Подключение к БД не выполнилось!\n";
+            await console.error(`${eMsg}\n${e}\n`);
+            return bot.sendMessage(chatID, eMsg);
+          }
+        }
+        const messageID = await states[chatID].messageID;
+        states[chatID] = await {};
+
+        return bot.editMessageText("Выберите действия с базой валютных пар", {
+          chat_id: chatID,
+          message_id: messageID,
+          ...BotOptions.adminActionEditCurrencies,
+        });
+      }
+
+      if (states[chatID].command == "editCurrency") {
+        states[chatID].command = await "";
+        states[chatID].runtime = await "";
+
+        try {
+          const currency = await CurrencyModel.findOne({
+            where: { currencyName: states[chatID].currencyName },
+          });
+          console.log(currency);
+          const ans = await msg.text;
+          if (states[chatID].editableField == "Name") {
+            currency.currencyName = await ans;
+          } else if (states[chatID].editableField == "APIKey") {
+            currency.currencyAPI = await ans;
+          } else if (states[chatID].editableField == "All") {
+            [currency.currencyName, currency.currencyAPI] = await ans.split(
+              ":"
+            );
+          }
+          await currency.save();
+          const messageID = await states[chatID].messageID;
+          states[chatID] = await {};
+
+          return bot.editMessageText("Выберите действия с базой валютных пар", {
+            chat_id: chatID,
+            message_id: messageID,
+            ...BotOptions.adminActionEditCurrencies,
+          });
+        } catch (e) {
+          // ОШИБКИ:
+          // Редактирование на уже существующее SequelizeUniqueConstraintError
+          // Нарушение ограничения длинны SequelizeDatabaseError
+          const eMsg =
+            await "Неудалось обратиться к БД для редактирования валютной пары!\n";
+          await console.error(`${eMsg}\n${e}\n`);
+          return bot.sendMessage(chatID, eMsg);
+        }
+      }
+    }
+
+    return bot.sendMessage(chatID, "Я вас не понимаю");
   });
 
   bot.on("callback_query", async (msg) => {
-    const data = await msg.data;
-    const chatID = await msg.from.id;
+    const data = await msg.data,
+      chatID = await msg.from.id,
+      messageID = await msg.message.message_id;
     console.log(msg);
 
     if (data == "startFollow") {
       return bot.editMessageText("Что вы хотите добавить в отслеживаемые", {
-        chat_id: msg.from.id,
-        message_id: msg.message.message_id,
+        chat_id: chatID,
+        message_id: messageID,
         ...BotOptions.allActionShowAllUnfollowed,
       });
       // return bot.editMessageReplyMarkup(BotOptions.allActionShowAllUnfollowed, {
       //   chat_id: msg.from.id,
-      //   message_id: msg.message.message_id,
+      //   message_id: messageID,
       // });
       // const message = await "Что вы хотите добавить в отслеживаемые";
       // return bot.sendMessage(
@@ -113,23 +185,37 @@ const start = async () => {
       // );
     }
     if (data == "stopFollow") {
-      const message = await "Что вы хотите удалить из отслеживаемых";
-      return bot.sendMessage(chatID, message, BotOptions.allActionShowFollowed);
+      // const message = await "Что вы хотите удалить из отслеживаемых";
+      // return bot.sendMessage(chatID, message, BotOptions.allActionShowFollowed);
+      return bot.editMessageText("Что вы хотите удалить из отслеживаемых", {
+        chat_id: chatID,
+        message_id: messageID,
+        ...BotOptions.allActionShowFollowed,
+      });
     }
     if (data == "useEditMode") {
       try {
         const user = await UsersModel.findOne({ where: { chatID: chatID } });
         if (user.rules == "ADMIN") {
-          return bot.sendMessage(
-            chatID,
-            "Что вы хотите редактировать",
-            BotOptions.adminActionEditDB
-          );
+          // return bot.sendMessage(
+          //   chatID,
+          //   "Что вы хотите редактировать",
+          //   BotOptions.adminActionEditDB
+          // );
+          return bot.editMessageText("Что вы хотите редактировать", {
+            chat_id: chatID,
+            message_id: messageID,
+            ...BotOptions.adminActionEditDB,
+          });
         } else {
           const eMsg =
             await "У вас недостаточно прав, чтобы совершать редактирование";
           console.error(eMsg);
-          return bot.sendMessage(chatID, eMsg);
+          // return bot.sendMessage(chatID, eMsg);
+          return bot.editMessageText(eMsg, {
+            chat_id: chatID,
+            message_id: messageID,
+          });
         }
       } catch (e) {
         const eMsg =
@@ -138,6 +224,267 @@ const start = async () => {
         return bot.sendMessage(chatID, eMsg);
       }
     }
+
+    if (data == "editCurrency") {
+      //добаить проверку на админку и вынести в отдельную функцию
+      // states[chatID] = await "editCurrency";
+      // return bot.editMessageText("Введите ")
+      return bot.editMessageText("Выберите действия с базой валютных пар", {
+        chat_id: chatID,
+        message_id: messageID,
+        ...BotOptions.adminActionEditCurrencies,
+      });
+    }
+    if (data == "editSecurity") {
+      //добаить проверку на админку и вынести в отдельную функцию
+      // states[chatID] = await "editSecurity";
+      return bot.editMessageText("Выберите действия с базой ценных бумаг", {
+        chat_id: chatID,
+        message_id: messageID,
+        ...BotOptions.adminActionEditSecurities,
+      });
+    }
+
+    if (data == "addCurrency") {
+      // добаить проверку на админку и вынести в отдельную функцию
+      states[chatID] = await {
+        command: "addCurrency",
+        runtime: "message",
+        messageID: messageID,
+      };
+      return bot.editMessageText(
+        "Чтобы добавить валютную пару введите {пару:api_ключ}",
+        {
+          chat_id: chatID,
+          message_id: messageID,
+        }
+      );
+    }
+    if (data == "changeCurrency") {
+      // добаить проверку на админку и вынести в отдельную функцию
+      // states[chatID] = await "changeCurrency";
+      // return bot.editMessageText(
+      //   "Чтобы выбрать валютную пару для редактирования введите {пару}",
+      //   {
+      //     chat_id: chatID,
+      //     message_id: messageID,
+      //   }
+      // );
+      // const users = await (
+      //   await UsersModel.findAll()
+      // ).map((user) => user.dataValues);
+      // console.log(users);
+      const currency = await (
+        await CurrencyModel.findAll()
+      ).map((currency) => currency.dataValues.currencyName);
+      const currencyOPT = await currency.map((currency) => [
+        { text: currency, callback_data: currency },
+      ]);
+      const currencyOptions = await {
+        reply_markup: JSON.stringify({
+          inline_keyboard: [...currencyOPT],
+        }),
+      };
+      // console.log(currency);
+      // console.log(currencyOPT);
+      console.log(currencyOptions);
+
+      states[chatID] = await {
+        command: "editCurrency",
+        runtime: "callback_data",
+        messageID: messageID,
+      };
+
+      return bot.editMessageText(
+        "Выберите пару которую хотите отредактировать",
+        {
+          chat_id: chatID,
+          message_id: messageID,
+          ...currencyOptions,
+        }
+      );
+    }
+    if (data == "editCurrencyName") {
+      states[chatID] = await {
+        ...states[chatID],
+        command: "editCurrency",
+        runtime: "message",
+        editableField: "Name",
+      };
+      console.log(states);
+      return bot.editMessageText("Введите новое название валютной пары:", {
+        chat_id: chatID,
+        message_id: messageID,
+      });
+    }
+    if (data == "editCurrencyAPIKey") {
+      states[chatID] = await {
+        ...states[chatID],
+        command: "editCurrency",
+        runtime: "message",
+        editableField: "APIKey",
+      };
+      console.log(states);
+      return bot.editMessageText(
+        "Введите новое значение API ключа валютной пары:",
+        {
+          chat_id: chatID,
+          message_id: messageID,
+        }
+      );
+    }
+    if (data == "editCurrencyAllFields") {
+      states[chatID] = await {
+        ...states[chatID],
+        command: "editCurrency",
+        runtime: "message",
+        editableField: "All",
+      };
+      console.log(states);
+      return bot.editMessageText(
+        "Введите новые значения полей валютной пары {пару:api_ключ}:",
+        {
+          chat_id: chatID,
+          message_id: messageID,
+        }
+      );
+    }
+    if (data == "removeCurrency") {
+      // добаить проверку на админку и вынести в отдельную функцию
+      // states[chatID] = await "changeCurrency";
+      // return bot.editMessageText(
+      //   "Чтобы выбрать валютную пару для редактирования введите {пару}",
+      //   {
+      //     chat_id: chatID,
+      //     message_id: messageID,
+      //   }
+      // );
+      const currency = await (
+        await CurrencyModel.findAll()
+      ).map((currency) => currency.dataValues.currencyName);
+      const currencyOPT = await currency.map((currency) => [
+        { text: currency, callback_data: currency },
+      ]);
+      const currencyOptions = await {
+        reply_markup: JSON.stringify({
+          inline_keyboard: [...currencyOPT],
+        }),
+      };
+      // console.log(currency);
+      // console.log(currencyOPT);
+      console.log(currencyOptions);
+
+      states[chatID] = await {
+        command: "removeCurrency",
+        runtime: "callback_data",
+      };
+
+      return bot.editMessageText("Выберите пару которую хотите удалить", {
+        chat_id: chatID,
+        message_id: messageID,
+        ...currencyOptions,
+      });
+    }
+
+    if (data == "addSecurity") {
+    }
+    if (data == "changeSecurity") {
+    }
+    if (data == "removeSecurity") {
+    }
+
+    if (data == "backToAdminEditDB") {
+    }
+
+    if (data == "backToShowActions") {
+      const message = await "Выберите действие:";
+      try {
+        const user = await UsersModel.findOne({ where: { chatID: chatID } });
+        const options =
+          user.rules == "ADMIN"
+            ? BotOptions.adminActions
+            : user.rules == "MODERATOR"
+            ? BotOptions.moderatorActions
+            : user.rules == "USER"
+            ? BotOptions.userActions
+            : null;
+        return bot.editMessageText(message, {
+          chat_id: chatID,
+          message_id: messageID,
+          ...options,
+        });
+      } catch (e) {
+        const eMsg = await "Подключение к БД не выполнилось!\n";
+        await console.error(`${eMsg}\n${e}\n`);
+        await bot.sendMessage(chatID, eMsg);
+        return bot.editMessageText(message, {
+          chat_id: chatID,
+          message_id: messageID,
+          ...BotOptions.userActions,
+        });
+      }
+    }
+
+    if (states[chatID] && states[chatID].runtime == "callback_data") {
+      if (states[chatID].command == "editCurrency") {
+        states[chatID] = await {
+          messageID: messageID,
+          currencyName: data,
+        };
+        return bot.editMessageText("Что вы хотите отредактировать в записи:", {
+          chat_id: chatID,
+          message_id: messageID,
+          ...BotOptions.adminActionEditCurrenciesFields,
+        });
+      }
+
+      if (states[chatID].command == "removeCurrency") {
+        states[chatID] = await {};
+        try {
+          const currency = await CurrencyModel.findOne({
+            where: { currencyName: data },
+          });
+          console.log(currency);
+          await currency.destroy();
+
+          return bot.editMessageText("Выберите действия с базой валютных пар", {
+            chat_id: chatID,
+            message_id: messageID,
+            ...BotOptions.adminActionEditCurrencies,
+          });
+        } catch (e) {
+          const eMsg =
+            await "Неудалось обратиться к БД для редактирования валютной пары!\n";
+          await console.error(`${eMsg}\n${e}\n`);
+          return bot.sendMessage(chatID, eMsg);
+        }
+      }
+    }
+    // if (states[chatID] && states[chatID].command == "addCurrency") {
+    //   states[chatID].command = await "";
+    //   const [currencyName, currencyAPI] = await msg.text.split(":");
+    //   // await console.log(currencyName, currencyAPI);
+    //   try {
+    //     await CurrencyModel.create({
+    //       currencyName: currencyName,
+    //       currencyAPI: currencyAPI,
+    //     });
+    //   } catch (e) {
+    //     if (e.name == "SequelizeUniqueConstraintError") {
+    //       console.error(e);
+    //     } else {
+    //       const eMsg =
+    //         await "Неудалось создать запись нового пользователя в БД!Подключение к БД не выполнилось!\n";
+    //       await console.error(`${eMsg}\n${e}\n`);
+    //       return bot.sendMessage(chatID, eMsg);
+    //     }
+    //   }
+    //   return bot.editMessageText("Выберите действия с базой валютных пар", {
+    //     chat_id: chatID,
+    //     message_id: states[chatID].messageID,
+    //     ...BotOptions.adminActionEditCurrencies,
+    //   });
+    // }
   });
 };
 
