715b49149099dfc0ba0ecf7a8b4b00afa99b27aa
brendan <superden081@gmail.com>
2022-03-14 23:10:01 +0300

    added new formates for currencies

--- js/formaters.js
+++ js/formaters.js
@@ -2,12 +2,114 @@ const formatterUSD = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 });
 
 const formatterRUB = new Intl.NumberFormat("ru-RU", {
   style: "currency",
   currency: "RUB",
   minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 });
 
-export { formatterRUB, formatterUSD };
+const formatterEUR = new Intl.NumberFormat("de-DE", {
+  style: "currency",
+  currency: "EUR",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterCNY = new Intl.NumberFormat("zh-CN", {
+  style: "currency",
+  currency: "CNY",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterBYN = new Intl.NumberFormat("ru-RU", {
+  style: "currency",
+  currency: "BYN",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterHKD = new Intl.NumberFormat("zh-HK", {
+  style: "currency",
+  currency: "HKD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterGBP = new Intl.NumberFormat("en-GB", {
+  style: "currency",
+  currency: "GBP",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterTRY = new Intl.NumberFormat("tr-TR", {
+  style: "currency",
+  currency: "TRY",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterCHF = new Intl.NumberFormat("de-DE", {
+  style: "currency",
+  currency: "CHF",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterJPY = new Intl.NumberFormat("ja-JP", {
+  style: "currency",
+  currency: "JPY",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterKZT = new Intl.NumberFormat("kk-KZ", {
+  style: "currency",
+  currency: "KZT",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+export {
+  formatterRUB,
+  formatterUSD,
+  formatterEUR,
+  formatterCNY,
+  formatterBYN,
+  formatterHKD,
+  formatterGBP,
+  formatterTRY,
+  formatterCHF,
+  formatterJPY,
+  formatterKZT,
+};
+
+export default {
+  RUB: formatterRUB,
+  USD: formatterUSD,
+  EUR: formatterEUR,
+  CNY: formatterCNY,
+  BYN: formatterBYN,
+  HKD: formatterHKD,
+  GBP: formatterGBP,
+  TRY: formatterTRY,
+  CHF: formatterCHF,
+  JPY: formatterJPY,
+  KZT: formatterKZT,
+};
+// usd(Доллар США)
+// rub(Рубль России)
+// eur(Евро)
+// cny(Китайский Юань)
+// byn(Белорусский рубль)
+// hkd(Гонконгский доллар)
+// gbp(Фунт стерлингов)
+// try(Турецкая лира)
+// chf(Швейцарский франк)
+// jpy(Японская ена)
+// kzt(Казахские тенге)
